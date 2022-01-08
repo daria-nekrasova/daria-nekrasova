a4dedcfd6bf5398293db13387b97d7c0c7d932b7
glebovanton <workglebov@gmail.com>
2022-01-08 15:35:34 +0300

    Change design Dev

---
+++ DEV/vue-now-ui-kit-master/src/components/InstaButton.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  name: "InstaButton",
+};
+</script>
+
+<template>
+  <div class="b6_box">
+    <a <!-- onclick="fbq('track', 'Lead');" -->
+      <!--      href="https://salebot.pro/r/temaphot_1"-->
+      <!--      href="tg://resolve?domain=nenekrasova"-->
+      href="https://telegram.me/nenekrasova" class="bt" target="_blank" ><span
+        >Хочу инстафотосет</span
+      ></a
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
